8d184b3ce4ed467b49e9666609003afa623cab94
GUYtaro <guytarotori06@gmail.com>
2025-01-25 13:55:42 +0700

    10

---
+++ Assets/enmmy/attack.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class EnemyAttack : MonoBehaviour
+{
+    public float damage = 20f; // ความเสียหายที่ศัตรูทำได้
+    public float attackCooldown = 1.5f; // ระยะเวลาระหว่างการโจมตีแต่ละครั้ง
+
+    private float lastAttackTime; // เวลาโจมตีครั้งล่าสุด
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            TryAttack(other);
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            TryAttack(other);
+        }
+    }
+
+    private void TryAttack(Collider player)
+    {
+        if (Time.time >= lastAttackTime + attackCooldown)
+        {
+            PlayerHealth playerHealth = player.GetComponent<PlayerHealth>();
+            if (playerHealth != null)
+            {
+                playerHealth.TakeDamage(damage);
+                Debug.Log("Enemy attacked Player!");
+                lastAttackTime = Time.time; // บันทึกเวลาโจมตี
+            }
+        }
+    }
+}
